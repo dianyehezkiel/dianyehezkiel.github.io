04f7607103bfa655add917fc02b421d27ffa5994
dianyehezkiel <dianyehezkiel@gmail.com>
2021-01-08 12:00:03 +0700

    create production version of web

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Website under development
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon...
         </p>
         <a
           className="App-link"
